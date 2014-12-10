94d79cacf2047123bee76a46e7e2befeea0b78c4
Sayawan Devs <sayawan.devs@gmail.com>
2014-12-10 14:43:00 +0200

    Initial commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4x4",
+            "9x9"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 33);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 528);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(12, 381);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 124);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Random number generator is error prone. So please restart the game for the new ta" +
+    "ble.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose table size:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Row";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Column";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Area";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 523);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+
+
+
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        TextBox[] cells = new TextBox[16];
+        Panel[] panels = new Panel[4];
+        static Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void text_changed(object sender, EventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            Panel panel = textBox.Parent as Panel;
+
+            if (textBox.Text != "")
+            {
+                rowColCheck(textBox);
+                areaCheck(textBox, panel);
+            }
+        }
+
+        public void drawTable(int tableSize)
+        {
+            int sayi = 0;
+            int tsayi = 0;
+            int size = Convert.ToInt32(Math.Sqrt(tableSize));
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    panels[sayi] = new Panel();
+                    panels[sayi].Name = "panel" + (sayi + 1);
+                    panels[sayi].Location = new System.Drawing.Point(j * size * 55 + 140, i * size * 55 + 20);
+                    panels[sayi].Size = new System.Drawing.Size(size * 50, size * 50);
+                    panels[sayi].BackColor = Color.RoyalBlue;
+
+                    for (int ti = 0; ti < size; ti++)
+                    {
+                        for (int tj = 0; tj < size; tj++)
+                        {
+                            cells[tsayi] = new TextBox();
+                            cells[tsayi].Name = "txt" + tsayi;
+                            cells[tsayi].Width = 50;
+                            cells[tsayi].Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+                            cells[tsayi].Location = new Point(tj * 50, ti * 50);
+                            cells[tsayi].TextAlign = HorizontalAlignment.Center;
+                            cells[tsayi].TextChanged += new EventHandler(text_changed);
+
+                            panels[sayi].Controls.Add(cells[tsayi]);
+                            tsayi++;
+                        }
+                    }
+
+                    this.Controls.Add(panels[sayi]);
+                    sayi++;
+
+                }
+
+            }
+
+            randomData(tableSize);
+        }
+
+        // TO-DO having issue with creating random data.
+        public void randomData(int tableSize)
+        {
+            int total = tableSize * tableSize;
+            int count = 0;
+
+            for (int i = 0; i < tableSize; i++)
+            {
+                for (int j = 0; j < tableSize; j++)
+                {
+                    int rand1 = r.Next(0, total);
+                    int rand2 = r.Next(1, tableSize + 1);
+
+                    if (cells[rand1].Text == "")
+                    {
+                        if (count <= tableSize)
+                        {
+                            cells[rand1].Text = rand2.ToString();
+                            cells[rand1].Enabled = false;
+                        }
+                    }
+                count++;
+                }
+            }
+
+        }
+
+        public void clearTable()
+        {
+            foreach (Panel panel in panels)
+            {
+                // not sure if it is necessary
+                foreach (TextBox cell in cells)
+                {
+                    this.Controls.Remove(cell);
+                    //cell.Dispose();
+                }
+                this.Controls.Remove(panel);
+            }
+
+            cells = new TextBox[] { };
+            panels = new Panel[] { };
+        }
+
+        public void areaCheck(TextBox changedCell, Panel changedCellParent)
+        {
+            foreach (TextBox cell in changedCellParent.Controls)
+            {
+                if (cell.Name != changedCell.Name)
+                {
+                    //string valui = txtElem.Name;
+                    //Console.WriteLine(valui);
+
+                    if (cell.Text == changedCell.Text)
+                    {
+                        cell.BackColor = Color.Purple;
+                    }
+                }
+            }
+        }
+
+        public void rowColCheck(TextBox changedCell)
+        {
+            // initialize
+            // cc stands for ChangedCell
+
+            string ccVal = changedCell.Text;
+            Panel ccPanel = changedCell.Parent as Panel;
+
+            int ccParentX = ccPanel.Location.X;
+            int ccX = changedCell.Location.X;
+            int ccRow = ccParentX + ccX;
+
+            int ccParentY = ccPanel.Location.Y;
+            int ccY = changedCell.Location.Y;
+            int ccCol = ccParentY + ccY;
+
+            foreach (TextBox cell in cells)
+            {
+                if (cell.Name != changedCell.Name)
+                {
+                    Panel panel = cell.Parent as Panel;
+
+                    int parentX = panel.Location.X;
+                    int selfX = cell.Location.X;
+                    int row = parentX + selfX;
+
+                    int parentY = panel.Location.Y;
+                    int selfY = cell.Location.Y;
+                    int col = parentY + selfY;
+
+                    string self = cell.Text;
+
+                    if (ccVal == self)
+                    {
+                        if (ccCol == col)
+                        {
+                            cell.BackColor = Color.Red;
+                        }
+
+                        if (ccRow == row)
+                        {
+                            cell.BackColor = Color.Pink;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            clearTable();
+
+            Console.WriteLine("selected" + comboBox1.SelectedItem.GetType());
+            string choice = comboBox1.SelectedItem.ToString();
+            int boardSize = int.Parse(choice.Substring(0, 1));
+
+            // set form size
+            this.Size = new System.Drawing.Size(boardSize * 55 + 180, boardSize * 55 + 80);
+            panel1.Height = this.Height;
+
+            cells = new TextBox[boardSize * boardSize];
+            panels = new Panel[boardSize];
+
+            drawTable(boardSize);
+
+        }
+ 
+
+        //public void horizontalCheck(int row, int col, int area, int num, string name)
+        //{
+        //    foreach (TextBox neme in txt)
+        //    {
+        //        if (neme.Name != name)
+        //        {
+        //            int rowa = int.Parse(neme.Name.Substring(3, 1));
+        //            int cola = int.Parse(neme.Name.Substring(4, 1));
+        //            int areaa = row + col;
+        //            int numa;
+
+        //            if (neme.Text != "")
+        //            {
+        //                numa = int.Parse(neme.Text);
+        //            }
+        //            else
+        //            {
+        //                continue;
+        //            }
+
+        //            if (rowa == row && numa == num)
+        //            {
+        //                neme.BackColor = Color.Red;
+        //            }
+
+        //            if (cola == col && numa == num)
+        //            {
+        //                neme.BackColor = Color.RoyalBlue;
+        //            }
+
+        //            areaa = areaa / 4;
+        //            area = area / 4;
+
+        //            MessageBox.Show(areaa + " " + area);
+
+        //            double num1 = Math.Round(2.1, 0);
+        //            double num2 = Math.Round(2.3, 0);
+
+        //            if (num1 == num2)
+        //            {
+        //                neme.BackColor = Color.SaddleBrown;
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
